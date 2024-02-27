1ea14ee07bcdc9376d68f48e3779a8d92b156eb7
Lan StoneElm <stone_elm@163.com>
2024-02-27 09:51:35 +0800

    一个新的干净的 springboot 2.1.3.RELEASE 项目

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SpringBootDemo
+# springboot-demo
 一个练习用的SpringBoot项目,包括版本兼容，后端技术
---
+++ src/test/java/com/stone/elm/springboot/demo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.stone.elm.springboot.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+}
