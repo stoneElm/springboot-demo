2fa83739013701d8a09d5e24054e429909688d86
Lan StoneElm <stone_elm@163.com>
2024-02-27 13:44:53 +0800

    添加配置--跨域问题

---
+++ src/main/java/com/stone/elm/springboot/demo/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.stone.elm.springboot.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    /**
+     * 跨域
+     * @return null
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource config = new UrlBasedCorsConfigurationSource();
+        config.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(config);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 允许任何域名访问
+        corsConfiguration.addAllowedOrigin("*");
+        // 允许任何header访问
+        corsConfiguration.addAllowedHeader("*");
+        // 允许任何方法访问
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+}
